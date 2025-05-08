bb9fdc865bb258a7cae65f4c15188c5094970a1c
elhamUnt <elham.hamiti@unt.edu.mk>
2025-05-08 13:22:42 +0000

    main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,43 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+class Matrica {
+public:
+  int matrix[4][4];
+
+  void shfaqMatricen() {
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        cout << matrix[i][j] << " ";
+      }
+      cout << endl;
+    }
+  }
+
+  int shumaDiagonales() {
+    int shuma = 0;
+    for (int i = 0; i < 4; i++) {
+      shuma += matrix[i][i];
+    }
+    return shuma;
+  }
+};
+
+int main() {
+  Matrica m;
+
+  cout << "Shkruani 16 elementet e matrices (4x4):" << endl;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      cin >> m.matrix[i][j];
+    }
+  }
+
+  cout << "Matrica juaj eshte:" << endl;
+  m.shfaqMatricen();
+
+  cout << "Shuma e diagonales kryesore eshte: " << m.shumaDiagonales() << endl;
+
+  return 0;
+}
